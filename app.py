29d949599fb00d42e5bdb15a312037efd60f3f62
dmoonat <moonatdeepak@gmail.com>
2021-05-29 08:07:22 +0530

    comments

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import requests
-
 from datetime import datetime,timedelta
 import time
 import json
+
+#used to create desktop notification, tested in linux OS
 import subprocess as s
 
 # importing twilio library
@@ -56,6 +57,7 @@ def notify(age,pincodes,actual_dates,notification_interval,sms=False):
 									if counter==0:
 										try:
 											if sms:
+												#pass list of twilio verified phone no., eg. send_sms(message,[987654321])
 												sent_sms("Vaccination slots available") 		  #sms via twilio	
 										except:
 											print('Something is wrong with Twilio')
@@ -86,11 +88,12 @@ def notify(age,pincodes,actual_dates,notification_interval,sms=False):
 		else:
 			return 0
 			
-
+		#increment the datetime by notification interval
 		dt = datetime.now() + timedelta(minutes=notification_interval)
 
 		try:
-			if datetime.now().strftime("%d-%m-%Y %H:%M") > actual_dates[0]+' 08:00': #if current date time is greater than 8:00AM 
+			#if current date time is greater than 8:00AM, don't consider current date for slots availability 
+			if datetime.now().strftime("%d-%m-%Y %H:%M") > actual_dates[0]+' 08:00':
 				actual_dates = actual_dates[1:]
 				if len(actual_dates)==1:
 					return 0
@@ -98,14 +101,15 @@ def notify(age,pincodes,actual_dates,notification_interval,sms=False):
 		except:
 			return 0
 
+		#wait for notification interval
 		while datetime.now()<dt:
-			time.sleep(1)
+			time.sleep(0.1)
 
 
 if __name__ == "__main__":
 	age = 24
 	pincodes = [] #eg. [123456]
-	num_days = 2 #including today
+	num_days = 2 #Current date + next 2 days
 	notification_interval = 1 #in minutes
 
 
@@ -113,11 +117,14 @@ if __name__ == "__main__":
 		current_dt = datetime.today()
 		print("Searching Covid vaccine slots",current_dt.strftime('%d-%m-%Y %H:%M'))
 
+		#eg. current_dt=28-05-2021, actual dates=[28-05-2021,29-05-2021,30-05-2021]
 		actual_dates = [(current_dt + timedelta(days=i)).strftime("%d-%m-%Y") for i in range(num_days+1)]
 	
 		flag =notify(age,pincodes,actual_dates,notification_interval,sms=False)	#set sms=True to sent notify via sms using Twilio api
+		
+		#once we received the notification, pause the script for 10 minutes
 		if flag:
-			time.sleep(300)
+			time.sleep(600)
+
 		print("*"*20)
 		time.sleep(1)
-
